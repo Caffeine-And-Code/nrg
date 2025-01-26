440a5ea7f6fd9e49fcdb2cf983e31ead39c666ab
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2025-01-26 01:57:48 +0100

    fidelity meter completed

--- resources/views/components/user-fidelitymeter.blade.php
+++ resources/views/components/user-fidelitymeter.blade.php
@@ -8,7 +8,7 @@
     <div class="progress fdMeter-container" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
         <div class="progress-bar fdMeter" style="width: {{ $advance }}%"></div>
     </div>
-    <div class="justify-content-right">
+    <div class="text-end">
         <h3 class="normalTextBold mt-1">€ {{ "$fdmeter" }}</h3>
     </div>
     <p class="smallTextRegular">{!! __("messages.howMuchLasts", ["lastM" => $lasting, "prize" => $fdprize]) !!}</p>
