9e0f6ac3cab8851595b5d3822ed8f1330ee483cf
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2025-01-26 01:51:47 +0100

    Added to pull

---
+++ resources/views/components/user-fidelitymeter.blade.php
@@ -0,0 +1,13 @@
+@php
+    $advance=100 * $user->last_meter / $fdmeter;
+    $lasting = $fdmeter - $user->last_meter;
+@endphp
+
+<section>
+    <h2 class="title mobile">{{ __("messages.fdMeter") }}</h2>
+    <div class="progress fdMeter-container" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+        <div class="progress-bar fdMeter" style="width: {{ $advance }}%"></div>
+    </div>
+    <h3 class="normalTextBold mt-1">€ {{ "$fdmeter" }}</h3>
+    <p class="smallTextRegular">{!! __("messages.howMuchLasts", ["lastM" => $lasting, "prize" => $fdprize]) !!}</p>
+</section>
